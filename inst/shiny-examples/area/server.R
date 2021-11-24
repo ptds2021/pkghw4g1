9f6eb90aa25a6278570829714a0813f9ec1ac861
Corinne Schönholzer <corinne.schonholzer@unil.ch>
2021-11-24 22:16:35 +0100

    Problem 3: Update file server.R with calcuations and plot for outputs

--- inst/shiny-examples/area/server.R
+++ inst/shiny-examples/area/server.R
@@ -1,26 +1,38 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
+
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
 #
 
 library(shiny)
+library(pkghw4g1)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
+    simulate <- reactive({
+        # simulate the area of S and measure the time here
+        options(digits.secs=3)
+        start.time <- Sys.time()
+        estimate_area(B = input$B, seed = input$seed)
+        end.time <- Sys.time()
+        execution_time <- end.time-start.time
+    })
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    output$plot <- renderPlot({
+        # plot area
+        plot(simulate())
+    })
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$time <- renderText({
+        # extract the time of the execution
+        paste("The necessary execution time was:", execution_time)
+    })
 
+    output$area <- renderText({
+        # extract the estimated value
+        area_s <- simulate()
+        paste("The estimated shape of the area is:", area_s)
     })
 
 })
