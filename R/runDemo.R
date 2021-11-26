67c95b61c470867b4f024998afe189782eba72cf
Remytombola <47745331+Remytombola@users.noreply.github.com>
2021-11-26 18:20:59 +0100

    create runDemo file for shiny app

---
+++ R/runDemo.R
@@ -0,0 +1,13 @@
+#' @export
+runDemo <- function() {
+  appDir <- system.file("shiny-examples", "area", package = "pkghw4g1")
+  if (appDir == "") {
+    stop(
+      "Could not find example directory. Try re-installing pkghw4g1.",
+      call. = FALSE
+    )
+  }
+
+  shiny::runApp(appDir, display.mode = "normal")
+
+}
